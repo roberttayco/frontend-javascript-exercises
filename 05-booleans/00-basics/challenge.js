3893e34ec8f96594db9ff4b991e5fa788b2121cf
Robert Tayco <rob@roberttayco.com>
2015-02-24 23:00:42 -0800

    booleans checkpoint

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,19 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+    return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+    return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+    return (lower < middle) && (middle < upper);
 };
 
 module.exports.outsideRanges = function(number) {
-
+    return ((number < 10) || (20 < number)) && ((number <= 42) || (75 < number)) && ((number <= 1) || (6 <= number)); 
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+    return (name === 'NYTimes' || name === 'LATimes') && (price >= 1);
 };
