2d8d1f41c7c90043e0f8507a6b72ccdd72a8663a
roberttayco <rob@roberttayco.com>
2015-03-01 21:46:18 +0000

    started canIGet()

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,32 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+    var course = {
+        title: courseTitle,
+        duration: courseDuration,
+        students: courseStudents
+    };
+    return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+    // if the `object` already has a property called `newProp`, just return `object`
+    if (object.hasOwnProperty(newProp)) {
+        return object;
+    } else {
+        object.newProp = newValue;
+        return newObj;
+    }
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+    return 'Hello ' + letter.recipient + ',\n\n' + letter.msg + '\n\nSincerely,\n' + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+   var items = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+   };
+
+};
