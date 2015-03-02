396d7c2b82ec45e0b87e116af9c82c6af8502409
roberttayco <rob@roberttayco.com>
2015-03-02 08:07:42 +0000

    loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,16 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+    while (conditionalFn()) {
+        actionFn();
+    }
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arr) {
+    var i   = 0,
+        sum = 0;
+    while (i < arr.length) {
+        sum += arr[i];
+        i++;
+    }
+    return sum;
+};
 
