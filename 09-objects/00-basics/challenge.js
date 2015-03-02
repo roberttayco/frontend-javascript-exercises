8b3aed3e4c13337bee5da21f523e2a8630c33982
roberttayco <rob@roberttayco.com>
2015-03-02 05:00:42 +0000

    looping over arrays

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -12,8 +12,10 @@ module.exports.addProperty = function(object, newProp, newValue) {
     if (object.hasOwnProperty(newProp)) {
         return object;
     } else {
-        object.newProp = newValue;
-        return newObj;
+        // object.newProp
+        // object["propertyName"] == object.propertyName
+        object[newProp] = newValue;
+        return object;
     }
 };
 
@@ -22,11 +24,30 @@ module.exports.formLetter = function(letter) {
 };
 
 module.exports.canIGet = function(item, money) {
-   var items = {
-    'MacBook Air': 999,
-    'MacBook Pro': 1299,
-    'Mac Pro': 2499,
-    'Apple Sticker': 1
-   };
+    var items = {
+        'MacBook Air': 999,
+        'MacBook Pro': 1299,
+        'Mac Pro': 2499,
+        'Apple Sticker': 1
+    };
+    return items[item] <= money;
+    /*
+        var item = {
+            'MacBook Air': 999
+        };
 
+        return typeof item[item] === "undefined" ? false : item[item] <= money;
+    */
+
+    // does the item provided match a property name on the object
+    // item[item]
+    // item.hasOwnProperty(item)
+
+    /*
+        if (items[item]) {
+            // return true if in price range
+        } else {
+            return false;
+        }
+    */
 };
